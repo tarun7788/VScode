36666d9104e7eaa31b6a2e4ab29d3fe1f73f3c52
Tarun Saraswat <tarunsaraswat77@gmail.com>
2023-04-28 23:51:33 +0530

    commit

---
+++ Codechef/Motivation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int comp(int arr1[],int arr2[],int i){
+    
+}
+
+int main() {
+	int T,s,r;
+    int arrs[10];
+    int arrr[10];
+	cin>>T;
+	for (int i = 0; i < T; i++) {
+	    for (int i = 0; i <= 3; i++) {
+	    cin>>arrs[i]>>arrr[i];
+	    }
+	    for (int i = 0; i <= 3; i++) {
+	    cout<<arrs[i]<<endl<<arrr[i];
+	    }
+	    
+	}
+	return 0;
+}
