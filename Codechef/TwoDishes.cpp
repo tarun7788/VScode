af9dcf591355b42039336add7838b92ec4d1a148
Tarun Saraswat <tarunsaraswat77@gmail.com>
2023-04-29 23:56:02 +0530

    commit

--- Codechef/TwoDishes.cpp
+++ Codechef/TwoDishes.cpp
@@ -1,7 +1,22 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define endl "\n"
+#define ll long long int 
+
+void solve()
+{
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  cout << (a<= min(c, b+d) ? "YES\n" : "NO\n");
+}
+
 int main()
 {
-int n;
-cin>>n;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  //freopen("input.txt", "r", stdin);
+  int t;
+  cin >> t;
+  while(t--) solve();
 }
