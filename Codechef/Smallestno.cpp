7123db7790ef4a3d1d83c5c10eca6b2d194d70b5
Tarun Saraswat <tarunsaraswat77@gmail.com>
2023-05-04 19:53:55 +0530

    commit

---
+++ Codechef/Smallestno.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+for (int i = 0; i <n; i++)
+{
+   int x,y;
+   cin>>x>>y;
+   if (y==0){
+    cout<<x<<endl;
+   }
+   else cout<<x%y<<endl;
+
+}
+return 0;
+}
