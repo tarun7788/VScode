343c89b3b2c759f4ad2aedc80af0a96167cfa865
Tarun Saraswat <tarunsaraswat77@gmail.com>
2023-05-07 22:50:44 +0530

    commit

---
+++ Codechef/distinctpair.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,count;
+
+	cin>>x;
+	for (int i = 0; i < x; i++) {
+	    int a,b;
+	    cin>>a>>b;
+	        cout<<((b-a)*2)+1<<endl;
+	    }
+	
+	return 0;
+}
